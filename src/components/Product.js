fb14c9fe60759d8a0cdb3c3e607a4fa8055087ea
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-05-19 21:49:37 +0430

    fix eye icon link for production, add real info to manifest.json, fix favicons in index.html

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,29 +1,39 @@
+import { useNavigate } from "react-router-dom";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="product-grid">
       <div className="product-image">
         <div className="image-container">
           <img className="image-1" alt={product.name} src={product.image1} />
         </div>
-        {product.hasDiscount && <span className="product-sale-label">Sale!</span>}
+        {product.hasDiscount && (
+          <span className="product-sale-label">Sale!</span>
+        )}
         <div className="price">{product.price} $</div>
-        <a href={`/products/${product.id}`} className="product-details">
+        <a
+          onClick={() => navigate(`/products/${product.id}`)}
+          className="product-details"
+        >
           <i className="fa fa-eye"></i>
         </a>
       </div>
       <div className="product-content">
-        <p className="title txt--black">
-          {product.name}
+        <p className="title txt--black">{product.name}</p>
+        <p className="description text-truncate">
+          {product.description.slice(0, 40)}
         </p>
-        <p className="description text-truncate">{product.description.slice(0, 40)}</p>
-        <Rating ratingNum={product.rating} reviewCount={product.numReviews} noText />
+        <Rating
+          ratingNum={product.rating}
+          reviewCount={product.numReviews}
+          noText
+        />
       </div>
     </div>
   );
 };
-
-// export default Product
 
 export default Product;

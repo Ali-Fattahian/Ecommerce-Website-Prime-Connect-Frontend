de59f6456792f912d54f81e91819d56a88565d22
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-06-11 16:35:01 +0430

    lazy load routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,68 @@
 import "./custom.scss";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage";
-import CartPage from "./pages/CartPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import ShippingPage from "./pages/ShippingPage";
-import PaymentPage from "./pages/PaymentPage";
-import PlaceOrderPage from "./pages/PlaceOrderPage";
-import AllProductsPage from "./pages/AllProductsPage";
 import Loader from "./components/Loader";
-import NoMatchPage from "./pages/NoMatchPage";
-import UsersListPage from "./pages/UsersListPage";
-import EditUserProfilePage from "./pages/EditUserProfilePage";
-import ProductsListPage from "./pages/ProductsListPage";
-import EditProductPage from "./pages/EditProductPage";
-import OrderListPage from "./pages/OrderListPage";
-import OrderPage from "./pages/OrderPage";
-import EnterCardInformationPage from "./pages/EnterCardInformationPage";
-import CreateProductPage from "./pages/CreateProductPage";
-import MessageManagementPage from "./pages/MessageManagementPage";
-import ReceivedMessageDetailPage from "./pages/ReceivedMessageDetailPage";
-import SentMessageDetailPage from "./pages/SentMessageDetailPage";
-import DashboadPage from "./pages/Admin/DashboadPage";
-import EnterEmailPasswordForgotPage from "./pages/EnterEmailPasswordForgotPage";
-import PasswordForgotCheckPage from "./pages/PasswordForgotCheckPage";
+
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const ProductPage = React.lazy(() => import("./pages/ProductPage"));
+const CartPage = React.lazy(() => import("./pages/CartPage"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
+const PlaceOrderPage = React.lazy(() => import("./pages/PlaceOrderPage"));
+const RegisterPage = React.lazy(() => import("./pages/RegisterPage"));
+const ShippingPage = React.lazy(() => import("./pages/ShippingPage"));
+const PaymentPage = React.lazy(() => import("./pages/PaymentPage"));
+const AllProductsPage = React.lazy(() => import("./pages/AllProductsPage"));
+const NoMatchPage = React.lazy(() => import("./pages/NoMatchPage"));
+const UsersListPage = React.lazy(() => import("./pages/UsersListPage"));
+const EditUserProfilePage = React.lazy(() =>
+  import("./pages/EditUserProfilePage")
+);
+const ProductsListPage = React.lazy(() => import("./pages/ProductsListPage"));
+const EditProductPage = React.lazy(() => import("./pages/EditProductPage"));
+const OrderListPage = React.lazy(() => import("./pages/OrderListPage"));
+const OrderPage = React.lazy(() => import("./pages/OrderPage"));
+const EnterCardInformationPage = React.lazy(() =>
+  import("./pages/EnterCardInformationPage")
+);
+const CreateProductPage = React.lazy(() => import("./pages/CreateProductPage"));
+const MessageManagementPage = React.lazy(() =>
+  import("./pages/MessageManagementPage")
+);
+const ReceivedMessageDetailPage = React.lazy(() =>
+  import("./pages/ReceivedMessageDetailPage")
+);
+const EnterEmailPasswordForgotPage = React.lazy(() =>
+  import("./pages/EnterEmailPasswordForgotPage")
+);
+const SentMessageDetailPage = React.lazy(() =>
+  import("./pages/SentMessageDetailPage")
+);
+const PasswordForgotCheckPage = React.lazy(() =>
+  import("./pages/ReceivedMessageDetailPage")
+);
+const DashboadPage = React.lazy(() => import("./pages/Admin/DashboadPage"));
 
 function App() {
   return (
     <Router>
       <Suspense>
         <Routes fallback={<Loader />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/enter-email/" element={<EnterEmailPasswordForgotPage />} />
-          <Route path="/change-password/:uidb64/:token" element={<PasswordForgotCheckPage />} />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<Loader />}>
+                <HomePage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/enter-email/"
+            element={<EnterEmailPasswordForgotPage />}
+          />
+          <Route
+            path="/change-password/:uidb64/:token"
+            element={<PasswordForgotCheckPage />}
+          />
           <Route path="products/:productId" element={<ProductPage />} />
           <Route path="/cart/:id" element={<CartPage />} />
           <Route path="cart/" element={<CartPage />} />
